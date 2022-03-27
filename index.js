047512938ae291ab28c194d84b5d0a20f3f19da7
Pyche Laveau <homedome@Seveonnos-MacBook-Air.local>
2022-03-27 14:06:34 -0700

    generate markdown templet

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-// TODO: Include packages needed for this application
+// DONE: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// DONE: Create a function to write README file 
+function writeToFile(fileName, data) => {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Your readMe was created! Go take a gander at the README.md file to see for yourself!");
+        }
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (licenseBadge !== "None") {
+    return "![Badge](https://img.shields.io/badge/License-"+licenseBadge.replace(/\s+/g, '')+"-yellow.svg)"
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (licenseBadge !== "None") {
+    return "This project is licensed under "+licenseBadge+ " ."
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,7 +24,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  return `
+  # ${data.title}
+  
+  ${renderLicenseBadge(data.licenseBadge)}
+
+  ## Description
+  ${data.description}
+
+  # Deployed Application 
+  [${data.url}](${data.url})
+  
+  ## Table of contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [License](#licence)
+  * [Contract](#contract)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  # Contributors
+  ${data.contributors}
+
+  #License
+  ${renderLicenseLink(data.licenseBadge)}
+
+  # Contract
+  If you have any questions or would like to contribute to this project, please feel free to reach me at @[${data.username}](https://github.com/${data.username}) or email me at ${data.email}
+
 `;
 }
 
